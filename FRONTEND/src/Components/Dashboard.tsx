af65d06a5e625c35d02a46c3be62602ed24b6993
Varunjoshi-19 <varunjoshi6283@gmail.com>
2025-02-14 15:25:15 +0530

    vote timer

--- FRONTEND/src/Components/Dashboard.tsx
+++ FRONTEND/src/Components/Dashboard.tsx
@@ -1,11 +1,47 @@
 import { useNavigate } from "react-router-dom";
 import styles from "../Styling/Dashboard.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 import logo from "../images/logo.jpeg";
 import voteNow from "../images/vote-now.png";
-import { Images } from "../script/GetImages"
+import { Images } from "../script/GetImages";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
   const navigate = useNavigate();
+  const [runningTime, setRunningTime] = useState<number>(Date.now());
+  const [hours, sethours] = useState<number>(0);
+  const [minute, setMinute] = useState<number>(0);
+  const [second, setSeconds] = useState<number>(0);
+  const [maxTime, setMaxTime] = useState<number>(Date.now() + 10000);
+
+  useEffect(() => {
+
+    const intervalId = setInterval(() => {
+      Timer();
+      const currentTime = Date.now();
+      setRunningTime(currentTime);
+      console.log(currentTime);
+
+      if (currentTime > maxTime) {
+        console.log("voting has been stopped");
+        clearInterval(intervalId);
+        return;
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+function Timer() {
+   
+}
 
   return (
     <>
@@ -38,24 +74,49 @@ function Dashboard() {
             flexDirection: "column",
             alignItems: "center",
             gap: "30px",
-            marginTop :"20px"
+            marginTop: "20px",
           }}
         >
           <img src={voteNow} alt="" id={styles.voteNow} />
           <span style={{ fontSize: "2rem" }}> 4 hr 50 min 45 sec</span>
         </div>
 
-        <div style={{ display :'flex' , flexDirection  :"column" , gap :"20px", marginTop :"20px" }}> 
-          
-           {Images.map((image) => (
-            <img src={image} alt="" style={{ width: "100px"  ,borderRadius : "50%" }} />
-          ))}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            marginTop: "20px",
+          }}
+        >
+          {Images.map((image, index) => (
+            <div key={index} className={styles.eachPartyInfo}>
+              <img
+                src={image.src}
+                alt=""
+                style={{ width: "100px", borderRadius: "50%" }}
+              />
 
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <span>{image.about}</span>
+                <span>Total votes : {image.votingCount}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
-      <footer>
+      <footer style={{ color: "white" }}>
         me sahil joshi creating footer
+        <FontAwesomeIcon icon={faFacebook} style={{ color: "#1877F2" }} />
+        <FontAwesomeIcon icon={faTwitter} style={{ color: "#1877F2" }} />
+        <FontAwesomeIcon icon={faInstagram} style={{ color: "#d62976" }} />
       </footer>
     </>
   );
--- FRONTEND/src/script/GetImages.ts
+++ FRONTEND/src/script/GetImages.ts
@@ -3,5 +3,28 @@ import party2 from "../images/party2.png.jpg";
 import party3 from "../images/party3.png.jpg";
 import party4 from "../images/party4.png.jpg";
 
+export const Images = [
+  {
+    src: party1,
+    votingCount: 50,
+    about: "inso party",
+  },
 
-export const Images = [party1 ,party2, party3 ,party4];
+  {
+    src: party2,
+    votingCount: 50,
+    about: "csf party",
+  },
+
+  {
+    src: party3,
+    votingCount: 50,
+    about: "soi party",
+  },
+
+  {
+    src: party4,
+    votingCount: 50,
+    about: "kcsu party ",
+  },
+];
